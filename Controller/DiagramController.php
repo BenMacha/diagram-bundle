0d1674f376e10013801d7ac0a88daea8cedd0abc
D'Ali Ben Macha <contact@benmacha.tn>
2018-03-09 16:20:49 +0100

    clean code

--- Controller/DiagramController.php
+++ Controller/DiagramController.php
@@ -2,6 +2,7 @@
 
 namespace benmacha\DiagramBundle\Controller;
 
+use Doctrine\ORM\Mapping\MappingException;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Doctrine\ORM\Mapping\ClassMetadata;
@@ -13,50 +14,6 @@ class DiagramController extends Controller
      */
     public function indexAction()
     {
-        return $this->render('DiagramBundle:Default:index.html.twig');
-    }
-
-    /**
-     * @Route("/ssss")
-     */
-    public function isndexAction()
-    {
-        $associationArray = array(1 => 'OneToOne', 2 => 'ManyToOne', 4 => 'OneToMany', 8 => 'ManyToMany');
-
-        $entities = array();
-        $association = array();
-        $em = $this->getDoctrine()->getManager();
-
-        $tables = $em->getMetadataFactory()->getAllMetadata();
-
-        /** @var ClassMetadata $table */
-        foreach ($tables as $table) {
-            $tableNameArray = explode('\\', $table->getName());
-
-            $tableName = $tableNameArray[count($tableNameArray) - 1];
-            $tmp = array();
-            foreach ($table->getFieldNames() as $columnKey => $columnValue) {
-                $tmp[$columnValue] = $table->getFieldMapping($columnValue)['type'];
-            }
-            $entities[$tableName] = $tmp;
-            $tmp = array();
-            foreach ($table->getAssociationMappings() as $assosKey => $assosValue) {
-                $targetEntity = explode('\\', $assosValue['targetEntity']);
-                $sourceEntity = explode('\\', $assosValue['sourceEntity']);
-
-                $tmp['targetEntity'] = $targetEntity[count($targetEntity) - 1];
-                $tmp['sourceEntity'] = $sourceEntity[count($sourceEntity) - 1];
-                $tmp['fieldName'] = $assosValue['fieldName'];
-                $tmp['mappedBy'] = $assosValue['mappedBy'];
-
-                $association[$associationArray[$assosValue['type']]][] = $tmp;
-            }
-        }
-
-        dump($association);
-        dump($entities);
-        die;
-
         return $this->render('DiagramBundle:Default:index.html.twig');
     }
 
@@ -80,7 +37,10 @@ class DiagramController extends Controller
           $tableName = $tableNameArray[count($tableNameArray) - 1];
           $tmp = array();
           foreach ($table->getFieldNames() as $columnKey => $columnValue) {
-              $tmp[$columnValue] = $table->getFieldMapping($columnValue)['type'];
+              try {
+                  $tmp[$columnValue] = $table->getFieldMapping($columnValue)['type'];
+              } catch (MappingException $e) {
+              }
           }
           $entities[$tableName] = $tmp;
           $tmp = array();
