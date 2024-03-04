ea14724c526b5746e6a7eff379a00f532a7c80fd
D'Ali Ben Macha <contact@benmacha.tn>
2024-03-04 13:41:33 +0100

    Update DiagramController.php

--- src/Controller/DiagramController.php
+++ src/Controller/DiagramController.php
@@ -2,10 +2,7 @@
 
 namespace Benmacha\DiagramBundle\Controller;
 
-use App\Services\MenuExtractorService;
 use Doctrine\ORM\EntityManagerInterface;
-use Doctrine\ORM\Mapping\MappingException;
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Doctrine\ORM\Mapping\ClassMetadata;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -38,36 +35,31 @@ class DiagramController
         $associations = array();
 
         $tables = $entityManager->getMetadataFactory()->getAllMetadata();
-dump($tables);
 
+        /** @var ClassMetadata $table */
+        foreach ($tables as $table) {
+            $tableName = $table->getName();
+            $fileds = array();
+            foreach ($table->getFieldNames() as $fieldName) {
+                $filedMapping = $table->getFieldMapping($fieldName);
+                $fileds[$filedMapping["columnName"]] = $table->getFieldMapping($fieldName)['type'];
+            }
+            $entities[$tableName] = $fileds;
+            foreach ($table->getAssociationMappings() as $association) {
+                $associations[$associationType[$association['type']]][] = array(
+                    'targetEntity' => $association['targetEntity'],
+                    'sourceEntity' => $association['sourceEntity'],
+                    'fieldName' => $association['fieldName'],
+                    'mappedBy' => $association['mappedBy'],
+                );
+            }
 
+        }
 
-
-
-      /** @var ClassMetadata $table */
-      foreach ($tables as $table) {
-          $tableName = $table->getName();
-          $fileds = array();
-          foreach ($table->getFieldNames() as $fieldName) {
-              $filedMapping = $table->getFieldMapping($fieldName);
-              $fileds[$filedMapping["columnName"]] = $table->getFieldMapping($fieldName)['type'];
-          }
-          $entities[$tableName] = $fileds;
-          foreach ($table->getAssociationMappings() as $association) {
-              $associations[$associationType[$association['type']]][] = array(
-                  'targetEntity' => $association['targetEntity'],
-                  'sourceEntity' => $association['sourceEntity'],
-                  'fieldName' => $association['fieldName'],
-                  'mappedBy' => $association['mappedBy'],
-              );
-          }
-
-      }
-
-      foreach ($associationType as $type){
-          if (isset($associations[$type]))
-          $associations[$type] = array_unique($associations[$type], SORT_REGULAR);
-      }
+        foreach ($associationType as $type){
+            if (isset($associations[$type]))
+                $associations[$type] = array_unique($associations[$type], SORT_REGULAR);
+        }
 
         return new Response($this->twig->render('@Diagram/Default/sample.html.twig', array(
             'entities' => $entities,
